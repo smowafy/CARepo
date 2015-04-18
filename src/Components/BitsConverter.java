f83cf1b73a14c325888cdd3f00d250f64b76a7e0
mayarali <mayar.bassel.ali@gmail.com>
2015-04-18 16:47:59 +0200

    added method Bits converter of arrays of different sizes

--- src/Components/BitsConverter.java
+++ src/Components/BitsConverter.java
@@ -3,20 +3,33 @@ package Components;
 public class BitsConverter {
 	public int[] IntegerToBits(int inp) {
 		int[] result = new int[32];
-		for(int i = 0; i < 32; i++) {
+		for (int i = 0; i < 32; i++) {
 			int bit = (inp & (1 << i));
-			if (bit > 0) result[32 - 1 - i] = 1;
-			else result[32 - 1 - i] = 0;
+			if (bit > 0)
+				result[32 - 1 - i] = 1;
+			else
+				result[32 - 1 - i] = 0;
 		}
 		return result;
 	}
+
+	public int BitsToInterger(int[] inp, int size) {
+		int out = 0;
+		for (int i = size - 1; i >= 0; i--) {
+			out += Math.pow(2, size - 1 - i) * inp[i];
+		}
+		return out;
+
+	}
+
 	public int BitsToInteger(int[] inp) {
 		int out = 0;
-		for(int i = 31; i>= 0; i--) {
-			out += Math.pow(2, 31-i)*inp[i];
+		for (int i = 31; i >= 0; i--) {
+			out += Math.pow(2, 31 - i) * inp[i];
 		}
 		return out;
 	}
+
 	public static void main(String[] args) {
 		BitsConverter x = new BitsConverter();
 		int[] arr = new int[32];
@@ -26,7 +39,7 @@ public class BitsConverter {
 		System.out.println(x.BitsToInteger(arr));
 		int a = 7;
 		arr = x.IntegerToBits(a);
-		for(int i = 31; i > 26; i--) {
+		for (int i = 31; i > 26; i--) {
 			System.out.print(arr[i]);
 		}
 	}
