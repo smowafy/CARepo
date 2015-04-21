7e48d623f75b3d80bf99ec97719411cf71ed3ed6
Omar Abdel-Wanis <OmarWanis@Omars-MacBook-Pro.local>
2015-04-21 20:36:24 +0200

    constructor added

--- src/WriteBack/WriteBack.java
+++ src/WriteBack/WriteBack.java
@@ -1,5 +1,13 @@
 package WriteBack;
 
+import Components.BitsConverter;
+
 public class WriteBack {
+
+	BitsConverter converter;
+
+	public WriteBack() {
+		converter = new BitsConverter();
+	}
 
 }
