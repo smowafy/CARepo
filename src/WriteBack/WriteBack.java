97f5e8e1893638b000dcf2a68ad409c9c3d29e22
Omar Abdel-Wanis <OmarWanis@Omars-MacBook-Pro.local>
2015-04-21 20:41:00 +0200

    writeToRegister added

--- src/WriteBack/WriteBack.java
+++ src/WriteBack/WriteBack.java
@@ -3,11 +3,18 @@ package WriteBack;
 import Components.BitsConverter;
 
 public class WriteBack {
-
+	Hashtable<Integer, Integer> registers;
 	BitsConverter converter;
 
 	public WriteBack() {
+		registers = new Hashtable<Integer, Integer>();
 		converter = new BitsConverter();
+	}
+
+	public void writeToRegister(int[] register, int[] value) {
+		registerAddress = converter.BitsToInteger(register);
+		registerValue = converter.BitsToInteger(value);
+		registers.put(registerAddress, registerValue);
 	}
 
 }
