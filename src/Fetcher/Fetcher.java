0deeec56eb06aea1d77509b10a0ae76a30426e3e
Omar Abdel-Wanis <OmarWanis@Omars-MacBook-Pro.local>
2015-04-23 14:21:40 +0200

    fetch class

--- src/Fetcher/Fetcher.java
+++ src/Fetcher/Fetcher.java
@@ -17,32 +17,12 @@ public class Fetcher {
 	BitsConverter converter;
 
 	public Fetcher() {
-		instructionMemory = Mips.Mips.instructionMemory;
-		adder = new Adder();
-		converter = new BitsConverter();
-		// PC = counter;
-		dataMemory = Mips.Mips.dataMemory;
-		regFile = Mips.Mips.regFile;
-		int check = fetchNext();
-		System.out.println(check);
-		if (check != 0) {
-			System.out.println("IF/ID:");
-			System.out.println("    "+fetch);
+			//type your code here
 			new Decoder(fetch);
 			new Fetcher();
-		}
 	}
 
 	private int fetchNext() {
-		int[] instruction = instructionMemory.getFromMemory(Mips.Mips.PC);
-		int check = converter.BitsToInteger(instruction);
-		if (check != 0) {
-			int[] count = converter.IntegerToBits(Mips.Mips.PC, 32);
-			count = adder.inc(count);
-			Mips.Mips.PC = converter.BitsToInteger(count);
-			fetch.insert(count);
-			fetch.insert(instruction);
-		}
-		return check;
+		
 	}
 }
