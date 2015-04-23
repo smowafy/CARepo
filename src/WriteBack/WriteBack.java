3956e20703ce85e44e3dcf18066767e41ea749b2
Omar Abdel-Wanis <OmarWanis@Omars-MacBook-Pro.local>
2015-04-23 15:49:37 +0200

    added print statements in writeback

--- src/WriteBack/WriteBack.java
+++ src/WriteBack/WriteBack.java
@@ -1,5 +1,7 @@
 package WriteBack;
 
+import java.util.Arrays;
+
 import Components.MUX;
 import Components.BitsConverter;
 import Components.Register;
@@ -10,8 +12,8 @@ public class WriteBack {
 	RegisterFile registerFile;
 	BitsConverter converter;
 	MUX inputSrc;
+
 	/* number of clock cycles = 4 + wbCalls */
-	
 
 	public WriteBack(Register instruction) {
 		registerFile = Mips.Mips.regFile;
@@ -34,7 +36,7 @@ public class WriteBack {
 		boolean memToReg = (instruction[69] == 1) ? true : false;
 		/* if register write = 0 then nothing to do */
 		if (regWrite) {
-			for (int j = 5; j >= 0; j--, i--) {
+			for (int j = 4; j >= 0; j--, i--) {
 				targetRegister[j] = instruction[i];
 			}
 			/* if ALU input, skip memory part */
@@ -43,13 +45,35 @@ public class WriteBack {
 			for (int j = 31; j >= 0; j--, i--) {
 				registerValue[j] = instruction[i];
 			}
+			System.out.println("ID/EX Register Components in order:");
+			System.out.println("Register Value: "+Arrays.toString(registerValue));
+			System.out.println("Target Register: "+Arrays.toString(targetRegister));
+			System.out.println("Memory To Register: "+memToReg);
+			System.out.println("Register Write: "+regWrite);
 			/* write into register file */
 			registerFile.insertIntoRegister(registerValue, targetRegister);
 		}
+		else
+			System.out.println("Nothing to be written");
 
 	}
 
 	public static void main(String[] args) {
-		
-}
+		int[] p = new int[71];
+		p[70] = 1;
+		p[69] = 0;
+		p[64] = 1;
+		for(int i = 65; i < 69; i++) {
+			p[i] = 0;
+		}
+		for(int i = 32; i < 64; i++){
+			p[i] = 1;
+		}
+		for(int i = 0; i < 32; i++) {
+			p[i] = 0;
+		}
+		Register r = new Register(71);
+		r.insert(p);
+		new WriteBack(r);
+	}
 }
