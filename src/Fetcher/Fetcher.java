bfeb5996de1e654b98653beb52f36b43d0e29807
mayarali <mayar.bassel.ali@gmail.com>
2015-04-24 22:46:28 +0300

    removed params from constructor + check that not last instruction

--- src/Fetcher/Fetcher.java
+++ src/Fetcher/Fetcher.java
@@ -15,23 +15,29 @@ public class Fetcher {
 	Register fetch = new Register(64);
 	RegisterFile regFile;
 	BitsConverter converter;
-	public Fetcher(RegisterFile reg, DataMemory instrMemory, DataMemory mem) {
-		instructionMemory = instrMemory;
+	public Fetcher() {
+		instructionMemory = Mips.Mips.instructionMemory;
 		adder = new Adder();
 		converter = new BitsConverter();
 		//PC = counter;
 		dataMemory = mem;
 		regFile = reg;
 		fetchNext();
-		new Decoder(regFile, fetch, dataMemory);
+		if(check != 0){
+		new Decoder(fetch);
+		new Fetch();
+		}
 	}
-	// Adding 2x1 Mux for selecting PC/BranchAdd
-	private void fetchNext() {
+	private int fetchNext() {
 			int [] instruction = instructionMemory.getFromMemory(Mips.Mips.PC);
+			int check = converter.IntegerToBits(instruction);
+			if(check!=0){
 			int [] count = converter.IntegerToBits(Mips.Mips.PC, 32);
 			count = adder.inc(count);
 			Mips.Mips.PC = converter.BitsToInteger(count);
 			fetch.insert(count);
 			fetch.insert(instruction);
+			}
+		return check;
 	}
 }
